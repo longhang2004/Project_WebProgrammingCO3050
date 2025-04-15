15345250a5113577732cc36ae99cdcec4e016e63
Phan Nguyễn Hữu Phước <138502336+phacko11@users.noreply.github.com>
2025-04-15 13:37:29 +0700

    add BlogList and BlogDetail page

---
+++ frontend/src/pages/public/BlogDetail.jsx
@@ -0,0 +1,10 @@
+function BlogList() {
+    return (
+      <div className="blog-list">
+        <h2>Danh sách bài viết</h2>
+        <p>Chào mừng bạn đến với blog của chúng tôi!</p>
+      </div>
+    )
+  }
+  
+export default BlogList;
